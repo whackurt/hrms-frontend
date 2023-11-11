637c6a7404593319823f87d6c79e0cc04d9e89ee
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-11-11 09:42:12 +0800

    feat: added validator in textfield

--- lib/widgets/text_field/text_field.dart
+++ lib/widgets/text_field/text_field.dart
@@ -4,8 +4,13 @@ class HRMSRoundedTextField extends StatefulWidget {
   final String? hintText;
   final TextEditingController? controller;
   final bool? obscureText;
+  final String? Function(String?)? validator;
   const HRMSRoundedTextField(
-      {super.key, this.hintText, this.controller, this.obscureText});
+      {super.key,
+      this.hintText,
+      this.controller,
+      this.validator,
+      this.obscureText});
 
   @override
   State<HRMSRoundedTextField> createState() => _HRMSRoundedTextFieldState();
@@ -14,16 +19,17 @@ class HRMSRoundedTextField extends StatefulWidget {
 class _HRMSRoundedTextFieldState extends State<HRMSRoundedTextField> {
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      obscureText: widget.obscureText == null ? false : true,
-      controller: widget.controller,
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: Colors.white,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(50.0)),
-        hintText: '${widget.hintText}',
-      ),
-    );
+    return TextFormField(
+        obscureText: widget.obscureText == null ? false : true,
+        controller: widget.controller,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(50.0)),
+          hintText: '${widget.hintText}',
+        ),
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: widget.validator);
   }
 }
