3480cb7fd07c5d320025c96ac43f7d10b15fef00
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-12-03 22:18:37 +0800

    refactor: minimize app icon size

--- lib/core/theme/app_icon.dart
+++ lib/core/theme/app_icon.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 Image appIcon() {
   return const Image(
-    width: 200.0,
+    width: 150.0,
     image: AssetImage('assets/images/blue-icon.png'),
   );
 }
